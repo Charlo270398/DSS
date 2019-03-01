5663136635e43051aaa04e318248e595229d1f2c
Miguel Campello Roman <mcr75@alu.ua.es>
2019-03-01 11:54:56 +0100

    Implementando clases clinica, departamento y box

--- gestionClinica/app/Box.php
+++ gestionClinica/app/Box.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Box extends Model
 {
-    //
+    private $numero;
+
+    public function clinica(){
+        return $this->belongsTo('App\Clinica');
+    }
+
+    public function getNumero(){
+        return $this->numero;
+    }
+
+    public function setNumero($numero){
+        $this->numero = $numero;
+    }
 }
+?>
