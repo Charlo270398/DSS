caefd2e8ac58a68b0317121a71cd52f6aaec5d2f
Carlos <carloszamora270398@gmail.com>
2019-03-05 18:01:21 +0100

    seederBox

--- gestionClinica/database/seeds/BoxesTableSeeder.php
+++ gestionClinica/database/seeds/BoxesTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Box;
+use App\Clinica;
 
 function anyadirDep($cli, $nombreDep){
 
@@ -24,10 +25,23 @@ class BoxesTableSeeder extends Seeder
      */
     public function run()
     {
+        $clinica= Clinica::where('nombre', '=', 'Clinica Alicante')->first();
         // Borramos los datos de la tabla
         DB::table('boxes')->delete();
         
         // Añadimos una entrada a esta tabla
+        $box= new Box(['numero' => 1, 'id_clinica' => $clinica->id]);
+        $box->save();
+        $box= new Box(['numero' => 2, 'id_clinica' => $clinica->id]);
+        $box->save();
+        $box= new Box(['numero' => 3, 'id_clinica' => $clinica->id]);
+        $box->save();
+        $box= new Box(['numero' => 4, 'id_clinica' => $clinica->id]);
+        $box->save();
+        $box= new Box(['numero' => 5, 'id_clinica' => $clinica->id]);
+        $box->save();
+        $box= new Box(['numero' => 6, 'id_clinica' => $clinica->id]);
+        $box->save();
             
     }
 }
