644ce07b9fa944d4890befa1931530085d644641
charlo270398 <carloszamora270398@gmail.com>
2019-03-07 20:09:01 +0100

    Ejemplo vistas con link

---
+++ gestionClinica/resources/views/pag2.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title> Mi Web</title>
+    </head>
+    <body>
+        <h1>¡Hola, estás en la página <?php echo $num; ?>!</h1>
+        <a href="/">Volver a la página 1</a> 
+    </body>
+</html>
