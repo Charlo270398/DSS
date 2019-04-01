c65a77cb76d0e7d34e8ed317a1425ffca68839ab
charlo270398 <carloszamora270398@gmail.com>
2019-04-01 12:53:01 +0200

    Controller lista medicos aun por probar

--- gestionClinica/app/Http/Controllers/MedicosController.php
+++ gestionClinica/app/Http/Controllers/MedicosController.php
@@ -3,16 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Usuario;
+use App\Rol;
 
 class MedicosController extends Controller
 {
-    public function mostrarListaMedicos() {
-        
+    public function mostrarListaMedicos(){
+        $rol = Rol::where('nombre', '=', 'Medico')->first();
+        $users = Usuario::where('rol_id', '=', $rol->id)->get();
         return view('/medico/lista');
     }
 
-    public function mostrarDepartamento($id) {
-        
-        return view('/medico/medico');
+    public function mostrarListaMedicosDepartamento($id_dep){
+       //TODO
+    }
+
+    public function mostrarMedico($id) {
+        $user = Usuario::findOrFail($id);
+        return view('/medico/medico', ['medico' => $user]);
     }
 }
