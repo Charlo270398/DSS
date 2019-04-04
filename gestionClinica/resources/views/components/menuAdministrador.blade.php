aca41888f0f7dd9d75aca51c90a5498dd9782fce
charlo270398 <carloszamora270398@gmail.com>
2019-04-04 22:29:56 +0200

    Merge Carlos-Juan OK

--- gestionClinica/resources/views/components/menuAdministrador.blade.php
+++ gestionClinica/resources/views/components/menuAdministrador.blade.php
@@ -10,7 +10,7 @@
         <div>
             <ul> <h2>Panel del administrador</h2></ul>
             <ul> <h4>Clínica: <a class="aPanel" href="/clinica/edit"> Editar </a>    </h4></ul>
-            <ul> <h4>Departamentos:  <a class="aPanel" href="/departamentos/add"> Añadir </a> <a class="aPanel" href="/departamentos/editList"> Editar </a> <a class="aPanel" href="/departamentos/deleteList"> Borrar </a>    </h4></ul>
+            <ul> <h4>Departamentos:  <a class="aPanel" href="/departamento/add"> Añadir </a> <a class="aPanel" href="/departamentos/editList"> Editar </a> <a class="aPanel" href="/departamentos/deleteList"> Borrar </a>    </h4></ul>
             <ul> <h4>Boxes: <a class="aPanel" href="/box/add"> Añadir </a> <a class="aPanel" href="/box/delete"> Borrar </a>    </h4></ul>
             <ul> <h4>Medicos: <a class="aPanel" href="/medico/add"> Dar de alta </a> <a class="aPanel" href="/medico/editList"> Editar </a>  <a class="aPanel" href="/medico/deleteList"> Borrar </a>   </h4></ul>
         </div>
