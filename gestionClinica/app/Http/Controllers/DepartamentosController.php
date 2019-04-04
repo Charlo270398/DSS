60d842873d94057304b6690d691513d204b53ef1
VDI Ubuntu 16.04 LTS EPS 2017 <alu@VDI-Ubuntu-EPS-2017>
2019-04-04 22:12:29 +0200

    se viene

--- gestionClinica/app/Http/Controllers/DepartamentosController.php
+++ gestionClinica/app/Http/Controllers/DepartamentosController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\BL\DepartamentoDAO;
 use App\BL\UserDAO;
-
+use App\Departamento;
 class DepartamentosController extends Controller
 {
     public function mostrarListaDepartamentos() {
-        
+
         $d = new DepartamentoDAO();
         return view('/departamento/lista', ['departamentos' => $d->mostrarListaDepartamentos()]); //La devuelve Alfabeticamente por defecto
     }
@@ -17,9 +17,26 @@ class DepartamentosController extends Controller
     public function mostrarDepartamento($id) {
 
         $d = new DepartamentoDAO();
-     
+
 
         return view('/departamento/departamento', ['departamento' => $d->mostrarDepartamento($id),'medicos' => $d->mostrarMedicosDepartamento($id)]);
+    }
+    public function mostrarAddForm() {
+        return view('/departamento/add', ['clinica' => 1] );
+    }
+    public function addDepartamento(Request $request) {
+        $b = new DepartamentoDAO();
+        $box = new Departamento();
+        $box->id = $request->input('id');
+        $box->nombre = $request->input('nombre');
+        $box->imagen = $request->input('imagen');
+        $box->clinica_id = $request->input('clinica_id');
+
+        if($b->addDepartamento($box)){
+            return view('/departamento/add', ['departamento' => $box] );
+        }else{
+            //return view('/error', ['error' => 'Error añadiendo el departamento'] );
+        }
     }
 
 }
