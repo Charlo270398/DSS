c95e9d79f65415850fd9269dedfa10283ad01f32
charlo270398 <carloszamora270398@gmail.com>
2019-04-05 16:07:28 +0200

    Web

--- gestionClinica/routes/web.php
+++ gestionClinica/routes/web.php
@@ -54,7 +54,6 @@ Route::post('departamento/editar_create', [
     'uses' => 'DepartamentosController@addDepartamento'
 ]);
 
-
 Route::post('box/editar_create', [
     'uses' => 'BoxController@addBox'
 ]);
