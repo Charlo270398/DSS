ed52160b10afdbab1f535e3de6f5a624ac934e5f
VDI Ubuntu 16.04 LTS EPS 2017 <alu@VDI-Ubuntu-EPS-2017>
2019-05-14 21:13:33 +0200

    putogithub

--- gestionClinica/app/Http/Controllers/CitasController.php
+++ gestionClinica/app/Http/Controllers/CitasController.php
@@ -16,10 +16,10 @@ class CitasController extends Controller
 
         if (Auth::check()) {
             $idU = Auth::user()->id;
-            $c = new CitaDAO();  
-            $u = new UserDAO();   
+            $c = new CitaDAO();
+            $u = new UserDAO();
             $d = new DepartamentoDAO();
-            return view('/user/citas/cita', ['cita' => $c->mostrarCita($idC), 'medico' => $u->mostrarUsuario($c->mostrarCita($idC)->medico_id), 
+            return view('/user/citas/cita', ['cita' => $c->mostrarCita($idC), 'medico' => $u->mostrarUsuario($c->mostrarCita($idC)->medico_id),
             'departamento' => $d->mostrarDepartamento($c->mostrarCita($idC)->medico_id)]);
         }
         else{
@@ -29,9 +29,9 @@ class CitasController extends Controller
 
     public function mostrarConfirmarCita($dia, $hora, $idMedico){
 
-        //Hay que comprobar que la long de los string de dia y hora 
-        //Hay que comprobar que los dias son correctos 
-        //Hay que comprobar que las horas son correctas, solo a en punto, y 20 y menos 20 
+        //Hay que comprobar que la long de los string de dia y hora
+        //Hay que comprobar que los dias son correctos
+        //Hay que comprobar que las horas son correctas, solo a en punto, y 20 y menos 20
         $u = new UserDAO();
         $validado = true;
 
@@ -61,20 +61,20 @@ class CitasController extends Controller
         for($i=0; $i<7; $i++){ //7 proximos dias
             if($time == $dia && 'Saturday' != date("l", strtotime($time)) && 'Sunday' != date("l", strtotime($time))){ //El día está dentro del intervalo y no es Sabado ni Domingo
                 $validado = true;
-            } 
+            }
             $time =  Date('Y-m-d', strtotime("+1 days",strtotime($time)));
         }
 
         if (Auth::check() && $validado) {
             $idU = Auth::user()->id;
-            $c = new CitaDAO();  
+            $c = new CitaDAO();
             $d = new DepartamentoDAO();
             $cita = $c->generarCita($dia, $hora, $idMedico);
             if($u->mostrarRol($idU)->id==2){//ID 2 de paciente
-                if($cita == null){//Cita no disponible (porque no hay boxes)      
+                if($cita == null){//Cita no disponible (porque no hay boxes)
                     return $this->mostrarCitasDisponiblesError($idMedico, 'No hay boxes disponibles para esa fecha');
                 }else{
-                    return view('/user/citas/confirmar', ['cita' => $cita, 'medico' => $u->mostrarUsuario($idMedico), 
+                    return view('/user/citas/confirmar', ['cita' => $cita, 'medico' => $u->mostrarUsuario($idMedico),
                 'departamento' => $d->mostrarDepartamento($u->mostrarUsuario($idMedico)->departamento_id)]);
                 }
             }
@@ -88,35 +88,35 @@ class CitasController extends Controller
 
     public function confirmarCita($dia, $hora, $idMedico){
 
-        //Hay que comprobar que la long de los string de dia y hora 
-        //Hay que comprobar que los dias son correctos 
-        //Hay que comprobar que las horas son correctas, solo a en punto, y 20 y menos 20 
+        //Hay que comprobar que la long de los string de dia y hora
+        //Hay que comprobar que los dias son correctos
+        //Hay que comprobar que las horas son correctas, solo a en punto, y 20 y menos 20
 
         if (Auth::check()) {
             $idU = Auth::user()->id;
-            $c = new CitaDAO();  
-            $u = new UserDAO();   
+            $c = new CitaDAO();
+            $u = new UserDAO();
             $d = new DepartamentoDAO();
             $cita = $c->generarCita($dia, $hora, $idMedico);
-            if($cita == null){//Cita no disponible (porque no hay boxes)      
+            if($cita == null){//Cita no disponible (porque no hay boxes)
                 return $this->mostrarCitasDisponiblesError($idMedico, 'No hay boxes disponibles para esa fecha');
             }else{
-                return view('/user/citas/confirmar', ['cita' => $cita, 'medico' => $u->mostrarUsuario($idMedico), 
+                return view('/user/citas/confirmar', ['cita' => $cita, 'medico' => $u->mostrarUsuario($idMedico),
             'departamento' => $d->mostrarDepartamento($idMedico)]);
             }
-            
+
         }
     }
 
     public function borrarCita($idC) {
-        $c = new CitaDAO();  
-        return view('/user/citas/cita', ['cita' => $c->mostrarCita($idC), 'medico' => $u->mostrarUsuario($c->mostrarCita($idC)->medico_id), 
+        $c = new CitaDAO();
+        return view('/user/citas/cita', ['cita' => $c->mostrarCita($idC), 'medico' => $u->mostrarUsuario($c->mostrarCita($idC)->medico_id),
         'departamento' => $d->mostrarDepartamento($c->mostrarCita($idC)->medico_id)]);
     }
 
     public function mostrarCitasDisponibles($idM){
 
-        $c = new CitaDAO();  
+        $c = new CitaDAO();
         $items = $c->mostrarHorario($idM);
         if (Auth::check()) {
             $id = Auth::user()->id;
@@ -124,7 +124,7 @@ class CitasController extends Controller
             if($u->mostrarRol($id)->id==2){//ID PACIENTE = 2
                 return view('/user/citas/disponibles', ['error' =>'', 'fechas' => $items, 'idMedico' => $idM]);
             }else{
-                return view('/user/menuusuario', ['tipo' => $u->mostrarRol($id), 'error' =>'¡No puedes reservar citas porque no eres un paciente!']); 
+                return view('/user/menuusuario', ['tipo' => $u->mostrarRol($id), 'error' =>'¡No puedes reservar citas porque no eres un paciente!']);
             }
         }else{
             //Excepcion??
@@ -134,7 +134,7 @@ class CitasController extends Controller
 
     public function mostrarCitasDisponiblesError($idM, $error){//Básicamente copypaste de mostrarCitasDisponibles pero introduciendo datos en error
 
-        $c = new CitaDAO();  
+        $c = new CitaDAO();
         $items = $c->mostrarHorario($idM);
         if (Auth::check()) {
             $id = Auth::user()->id;
@@ -142,7 +142,7 @@ class CitasController extends Controller
             if($u->mostrarRol($id)->id==2){//ID PACIENTE = 2
                 return view('/user/citas/disponibles', ['error' =>$error, 'fechas' => $items, 'idMedico' => $idM]);
             }else{
-                return view('/user/menuusuario', ['tipo' => $u->mostrarRol($id), 'error' =>'No puedes reservar citas porque no eres un paciente!']); 
+                return view('/user/menuusuario', ['tipo' => $u->mostrarRol($id), 'error' =>'No puedes reservar citas porque no eres un paciente!']);
             }
         }else{
             //Excepcion??
@@ -155,7 +155,7 @@ class CitasController extends Controller
         if (Auth::check()) {
 
             $id = Auth::user()->id;
-           
+
             $cita = new Cita();
             $cita->medico_id = $request->input('idM');
             $cita->paciente_id = $id;
@@ -163,7 +163,7 @@ class CitasController extends Controller
             $cita->fecha = $request->input('fecha');
             $cita->motivo = $request->input('motivo');
             $cita->save();
-            
+
             return redirect('/citas&recientes');
         }else{
             return redirect('/home');
