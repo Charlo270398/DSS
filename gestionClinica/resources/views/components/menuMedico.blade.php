1ca7527a054e681a6c6a2dbe4f92d049ff3f42df
charlo270398 <carloszamora270398@gmail.com>
2019-05-20 10:11:05 +0200

    Menu medico

--- gestionClinica/resources/views/components/menuMedico.blade.php
+++ gestionClinica/resources/views/components/menuMedico.blade.php
@@ -23,8 +23,9 @@
     <?php } ?>
         <div class="container">
             <ul><h2><strong>Panel del médico</strong></h2></ul>
-            <ul><a class="aPanel" href="/citas&recientes"> Registro de citas </a></ul>
+            <ul><a class="aPanel" href="/citas&proximas"> Citas pendientes </a></ul>
             <ul><a class="aPanel" href="/citas&hoy"> Citas para hoy </a></ul>
+            <ul><a class="aPanel" href="/citas&recientes"> Historial de citas </a></ul>
             <ul><a class="aPanel" href="/pacientes"> Buscar ficha de paciente </a></ul>
         </div>
     </body>
