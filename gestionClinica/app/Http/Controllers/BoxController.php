095cc5994909367df8a47c09bcbd078cda0705c1
VDI Ubuntu 16.04 LTS EPS 2017 <alu@VDI-Ubuntu-EPS-2017>
2019-05-22 12:53:47 +0200

    añadiendo logs y seguridad

--- gestionClinica/app/Http/Controllers/BoxController.php
+++ gestionClinica/app/Http/Controllers/BoxController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use App\BL\UserDAO;
 use App\BL\BoxDAO;
 use App\Box;
 use App\Departamento;
@@ -11,7 +12,21 @@ use App\BL\DepartamentoDAO;
 class BoxController extends Controller
 {
     public function mostrarAddForm() {
-        return view('/box/add', ['clinica' => 1] );
+        $u = new UserDAO();
+        if (Auth::check()) {
+            $ida = Auth::user()->id;
+            if($u->mostrarRol($ida)->id==1){//ID PACIENTE = 2
+                return view('/box/add', ['clinica' => 1] );
+
+            }else{
+                error_log("Intento de acceso a add box sin tener permisos", 0);
+                return view('/user/menuusuario', ['citas'=>0 , 'tipo' => $u->mostrarRol($ida), 'error' =>'¡No puedes acceder  a esta parte porque no tienes permisos!']);
+            }
+        }else{
+            error_log("Intento de acceso sin haber iniciado sesión", 0);
+            return redirect('/login');
+        }
+
     }
     public function mostrarEditarForm($id) {
         $b = new BoxDAO();
@@ -24,9 +39,22 @@ class BoxController extends Controller
                 return view('/box/lista', ['departamentos' => $d->mostrarListaBoxes(), 'op' => 'mostrar']); //La devuelve Alfabeticamente por defecto
     }
     public function mostrarListaBoxBorrar() {
+        $u = new UserDAO();
+        if (Auth::check()) {
+            $ida = Auth::user()->id;
+            if($u->mostrarRol($ida)->id==1){//ID PACIENTE = 2
+                $d = new BoxDAO();
+                return view('/box/lista', ['departamentos' => $d->mostrarListaBoxes(), 'op' => 'borrar']); //La devuelve Alfabeticamente por defecto
 
-        $d = new BoxDAO();
-        return view('/box/lista', ['departamentos' => $d->mostrarListaBoxes(), 'op' => 'borrar']); //La devuelve Alfabeticamente por defecto
+            }else{
+                error_log("Intento de acceso a add box sin tener permisos", 0);
+                return view('/user/menuusuario', ['citas'=>0 , 'tipo' => $u->mostrarRol($ida), 'error' =>'¡No puedes acceder  a esta parte porque no tienes permisos!']);
+            }
+        }else{
+            error_log("Intento de acceso sin haber iniciado sesión", 0);
+            return redirect('/login');
+        }
+
     }
     public function borrarBox($id) {
         $d = new boxDAO();
